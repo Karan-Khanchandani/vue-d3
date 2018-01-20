affe15cc804de9af1175a93025379d8af532e10d
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-01-20 19:55:40 +0530

    Adds point and scatter template

---
+++ linechart/Point.vue
@@ -0,0 +1,49 @@
+<template id="d3__point">
+  <circle class="point" ref="point" :style="style"></circle>
+</template>
+
+<script>
+export default {
+  name: 'd3_point',
+  props: ['layout', 'point-data', 'scale', 'series-id'],
+  data () {
+
+  },
+  mounted () {
+      this.drawPoint();
+  },
+  methods : {
+      drawPoint: function() {
+          var scale = this.scale;
+
+          var $point = d3.select(this.$refs.point);
+          $point
+          .datum(this.pointData)
+          .attr('cx', function(d){
+              return scale.x(d.timestamp);
+          })
+          .attr('cy', function(d){
+              return scale.y(d.value);
+          })
+          .attr('r', 5);
+      }
+  },
+  computed: {
+      style: function() {
+        return{
+            fill: '#fff',
+            stroke: this.scale.color(this.seriesId),
+            strokeWidth: 2
+        }
+      }
+  },
+  watch:{
+      scale: {
+          deep: true,
+          handler: function(val, oldVal){
+              this.drawPoint()
+          }
+      }
+  }
+}
+</script>
---
+++ linechart/Scatter.vue
@@ -0,0 +1,27 @@
+<template id = "d3__scatter">
+  <g class = "points">
+      <d3__point
+        v-for="pointData in seriesData.values"
+        :key="pointData.id"
+        v-if="typeof pointData.value !== typeof null"
+        :series-id="seriesData.id"
+        :point-data="pointData"
+        :layout="layout"
+        :scale="scale"></d3__point>
+</template>
+<script>
+import Point from '@/Point';
+export default {
+  name: 'scatter',
+  data (){
+      return {}
+  },
+  props: ['layout', 'series-data', 'scale'],
+  watch: {
+      scale: {
+          deep:true,
+          handler: function(){}
+      }
+  }
+}
+</script>
